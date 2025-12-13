111aaad53567cb939e57ac65b29b7e9ea1209a6b
Manju Angadi <152906296+manjusangadi@users.noreply.github.com>
2025-12-13 21:30:11 +0530

    Implement countOdds method to count odd numbers

---
+++ 1523.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        # count = 0
+        # for i in range(low,high+1):
+        #     if i%2==1:
+        #         count+=1
+        # return count
+        odds_up_to_high = (high + 1) >> 1
+        odds_before_low = low >> 1
+        return odds_up_to_high - odds_before_low
