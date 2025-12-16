e2412e9f4dfa5904ece74ba30e2d08bd4b14784d
Manju Angadi <152906296+manjusangadi@users.noreply.github.com>
2025-12-16 23:45:12 +0530

    Implement in-place string reversal in reverseString
    Modify the reverseString method to use in-place reversal.

--- 344.py
+++ 344.py
@@ -10,4 +10,12 @@ class Solution:
             s[l], s[r] = s[r], s[l]
             l+=1
             r-=1
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        return s.reverse()
         
