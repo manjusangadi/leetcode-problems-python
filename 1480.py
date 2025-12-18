bb0dd91c6df93c005252e58f9a43b733c5989bea
Manju Angadi <152906296+manjusangadi@users.noreply.github.com>
2025-12-18 23:09:59 +0530

    Update 1480.py

--- 1480.py
+++ 1480.py
@@ -9,3 +9,11 @@ class Solution:
             res.append(current_sum)
         
         return res
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+
+        for i in range(1, len(nums)):
+            nums[i] +=nums[i-1]
+        return nums
